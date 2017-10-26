e9bb9c8e77132f56998ee4129bb2d4e385da4d26
Sharad Shanbhag <sshanbhag@neomed.edu>
2017-10-25 21:36:45 -0400

    analyzing 1155 while recording

--- RLFcomparesettings.m
+++ RLFcomparesettings.m
@@ -21,6 +21,7 @@ file2 = '1155_20171004_01_01_2563_BBN_LEVEL_optoON.dat';
 channelNumber = 8;
 %}
 
+%{
 % animal id 
 animalID = '1155';
 % date code (helps locate files in data directory)
@@ -30,18 +31,40 @@ dateID = '20171006';
 % file2 = '1155_20171006_03_03_2988_BBN_optoON_ch5.dat';
 file1 = '1155_20171006_04_03_3123_BBN_optoOFF_ch5ch11.dat';
 file2 = '1155_20171006_04_03_3123_BBN_optoON_ch5ch11_9.dat';
-
 % Channel Number (use 8 for single channel data)
 channelNumber = 5;
+%}
+
+%{
+% animal id 
+animalID = '1155';
+% date code (helps locate files in data directory)
+dateID = '20171025';
+% files to compare
+file1 = '1155_20171025_02_02_2397_BBN.dat';
+file2 = '1155_20171025_02_02_2397_BBN_optoON_7.dat';
+% Channel Number (use 8 for single channel data)
+channelNumber = 8;
+%}
+
+% animal id 
+animalID = '1155';
+% date code (helps locate files in data directory)
+dateID = '20171025';
+% files to compare
+% file1 = '1155_20171025_05_03_2616_BBN_2.dat';
+% file2 = '1155_20171025_05_03_2616_BBN_optoON_2.dat';
+file1 = '1155_20171025_05_03_2616_BBN_3.dat';
+file2 = '1155_20171025_05_03_2616_BBN_optoON_15.dat';
+% Channel Number (use 8 for single channel data)
+channelNumber = 8;
+% time window (post-stimulus onset) for analysis : this is added to stim
+% delay
+WindowLen = [0 100];
 
 %---------------------------------------------------------------------
 % settings for processing data
 %---------------------------------------------------------------------
-
-% time window (post-stimulus onset) for analysis : this is added to stim
-% delay
-WindowLen = [0 75];
-
 % filter
 HPFreq = 350;
 LPFreq = 6500;
