c54df491106cc6e4b2b14ee1f04c313c3d081d92
Sharad Shanbhag <sshanbhag@neomed.edu>
2017-10-27 17:00:54 -0400

    granting....
    need to add onset/offset lines

--- analyze1122_grantplots.m
+++ analyze1122_grantplots.m
@@ -84,7 +84,7 @@ datafile = file_list{1};
 								'yLimits', ylimits);
 							
 datafile = file_list{2};
-[D2, Dinf2, S2] = optoproc('file', fullfile(datapath, datafile), ...
+[D2, Dinf2, S2, T2] = optoproc('file', fullfile(datapath, datafile), ...
 								'plotpath', plotpath_base, ...
 								'channel', channelNumber, ...
 								'HPFreq', HPFreq, ...
@@ -141,23 +141,23 @@ binSize = 20;
 %% plot BBN data for file 1
 %------------------------------------------------------------------------
 %------------------------------------------------------------------------
-
+Dinf = Dinf1;
+T = T1;
+[~, fname] = fileparts(file_list{1});
 %---------------------------------------------------------------------
 % get list of stimuli (wav file names) and find BBN index (store in v)
 %---------------------------------------------------------------------
-varlist = Dinf1.test.wavlist;
-nvars = length(varlist);
+varlist = Dinf.test.wavlist;
 v = find(strcmpi('BBN', varlist));
 % sample rate
-Fs = Dinf1.indev.Fs;
+Fs = Dinf.indev.Fs;
 %---------------------------------------------------------------------
 % determine global max
 %---------------------------------------------------------------------
 % # of reps
-nreps = Dinf1.test.Reps;
 % find rms, max vals for each stim
-netrmsvals = rms(T1{v});
-maxvals = max(abs(T1{v}));
+netrmsvals = rms(T{v});
+maxvals = max(abs(T{v}));
 % compute overall mean rms for threshold
 fprintf('Calculating mean and max RMS for data...\n');
 mean_rms = mean(netrmsvals);
@@ -165,60 +165,11 @@ fprintf('\tMean rms: %.4f\n', mean_rms);
 % find global max value (will be used for plotting)
 global_max = max(maxvals);
 fprintf('\tGlobal max abs value: %.4f\n', global_max);
-
-%{
 %---------------------------------------------------------------------
 % find spikes!
 %---------------------------------------------------------------------
-% spiketimes = spikeschmitt2(T1{v}', spikeThreshold*mean_rms, 1, Fs);
-spiketimes = spikeschmitt2(T1{v}', -0.35*global_max, 1, Fs);
-% convert spike times in seconds to milliseconds
-for r = 1:length(spiketimes)
-	spiketimes{r} = (1000/Fs)*spiketimes{r};
-end
-
-% new figure
-hF = figure(10);
-% name figure
-[~, fname] = fileparts(file_list{1});
-set(hF, 'Name', [fname '_' varlist{v}]);
-
-%---------------------------------------------------------------------
-% Plot raw data
-%---------------------------------------------------------------------
-subplot(1, 3, 1)
-% time vector for plotting
-t = (1000/Fs)*((1:length(T1{v}(:, 1))) - 1);
-% flip tracesByStim in order to have sweeps match the raster plots
-stackplot(t, fliplr(T1{v}), 'colormode', 'black', ...
-										'ymax', global_max, ...
-										'figure', hF, 'axes', gca);
-title({fname, varlist{v}}, 'Interpreter', 'none');
-xlabel('ms')
-ylabel('Trial')
-% adjust y tick labels
-ytl = get(gca, 'YTickLabels');
-set(gca, 'YTickLabels', flipud(ytl))
-
-%---------------------------------------------------------------------
-% Plot raster data
-%---------------------------------------------------------------------
-subplot(1, 3, 2);
-rasterplot(spiketimes, [0 Dinf1.test.AcqDuration], '.', 16, 'k');
-
-%---------------------------------------------------------------------
-% Plot psth data
-%---------------------------------------------------------------------
-subplot(1, 3, 3)
-[histdata.H, histdata.bins] = psth(spiketimes, binSize, [0 Dinf1.test.AcqDuration]);
-bar(histdata.bins, histdata.H, 1, 'EdgeColor', 'b', 'FaceColor', 'b');
-%}
-
-%---------------------------------------------------------------------
-% find spikes!
-%---------------------------------------------------------------------
-% spiketimes = spikeschmitt2(T1{v}', spikeThreshold*mean_rms, 1, Fs);
-spiketimes = spikeschmitt2(T1{v}', -0.35*global_max, 1, Fs);
+% spiketimes = spikeschmitt2(T{v}', spikeThreshold*mean_rms, 1, Fs);
+spiketimes = spikeschmitt2(T{v}', -0.35*global_max, 1, Fs);
 % convert spike times in seconds to milliseconds
 for r = 1:length(spiketimes)
 	spiketimes{r} = (1000/Fs)*spiketimes{r};
@@ -227,19 +178,73 @@ end
 % psth
 %---------------------------------------------------------------------
 [histdata.H, histdata.bins] = psth(spiketimes, binSize, ...
-														[0 Dinf1.test.AcqDuration]);
-
+														[0 Dinf.test.AcqDuration]);
 %---------------------------------------------------------------------
 % plot data
 %---------------------------------------------------------------------
 % new figure
 hF = figure(10);
 % name figure
-[~, fname] = fileparts(file_list{1});
 set(hF, 'Name', [fname '_' varlist{v}]);
 % time vector for plotting
-t = (1000/Fs)*((1:length(T1{v}(:, 1))) - 1);
-grantplot(hF, t, T1{v}, global_max, spiketimes, histdata);
+t = (1000/Fs)*((1:length(T{v}(:, 1))) - 1);
+grantplot(hF, t, T{v}, global_max, spiketimes, histdata);
+% captions
+subplot(1, 3, 1)
+title({fname, varlist{v}}, 'Interpreter', 'none');
+
+%------------------------------------------------------------------------
+%------------------------------------------------------------------------
+%% plot BBN data for file 2
+%------------------------------------------------------------------------
+%------------------------------------------------------------------------
+Dinf = Dinf2;
+T = T2;
+[~, fname] = fileparts(file_list{2});
+%---------------------------------------------------------------------
+% get list of stimuli (wav file names) and find BBN index (store in v)
+%---------------------------------------------------------------------
+varlist = Dinf.test.wavlist;
+v = find(strcmpi('BBN', varlist));
+% sample rate
+Fs = Dinf.indev.Fs;
+%---------------------------------------------------------------------
+% determine global max
+%---------------------------------------------------------------------
+% find rms, max vals for each stim
+netrmsvals = rms(T{v});
+maxvals = max(abs(T{v}));
+% compute overall mean rms for threshold
+fprintf('Calculating mean and max RMS for data...\n');
+mean_rms = mean(netrmsvals);
+fprintf('\tMean rms: %.4f\n', mean_rms);
+% find global max value (will be used for plotting)
+global_max = max(maxvals);
+fprintf('\tGlobal max abs value: %.4f\n', global_max);
+%---------------------------------------------------------------------
+% find spikes!
+%---------------------------------------------------------------------
+% spiketimes = spikeschmitt2(T{v}', spikeThreshold*mean_rms, 1, Fs);
+spiketimes = spikeschmitt2(T{v}', -0.35*global_max, 1, Fs);
+% convert spike times in seconds to milliseconds
+for r = 1:length(spiketimes)
+	spiketimes{r} = (1000/Fs)*spiketimes{r};
+end
+%---------------------------------------------------------------------
+% psth
+%---------------------------------------------------------------------
+[histdata.H, histdata.bins] = psth(spiketimes, binSize, ...
+														[0 Dinf.test.AcqDuration]);
+%---------------------------------------------------------------------
+% plot data
+%---------------------------------------------------------------------
+% new figure
+hF = figure(11);
+% name figure
+set(hF, 'Name', [fname '_' varlist{v}]);
+% time vector for plotting
+t = (1000/Fs)*((1:length(T{v}(:, 1))) - 1);
+grantplot(hF, t, T{v}, global_max, spiketimes, histdata);
 % captions
 subplot(1, 3, 1)
 title({fname, varlist{v}}, 'Interpreter', 'none');
