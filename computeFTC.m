76eb3f6265094fb808469d2f7c04d1ac1b1bef6c
Sharad Shanbhag <sshanbhag@neomed.edu>
2022-01-04 13:33:48 -0500

    added return arg of bootstrap ci data
     bootData       struct with bootstat data returned by bootci for the  bootstrap estimates for mean and median confidence  intervals
    
    updated comments/docs

--- computeFTC.m
+++ computeFTC.m
@@ -1,6 +1,7 @@
 function varargout = computeFTC(spikeTimes, freqs, analysisWindow, varargin)
 %------------------------------------------------------------------------
 %  curvestruct = computeFTC(spikeTimes, freqs, analysisWindow, nbootstrap)
+%  [curvestruct, spikeCount, bootData] = computeFTC(...)
 %------------------------------------------------------------------------
 % TytoLogy:Experiments:OptoAnalysis
 %------------------------------------------------------------------------
@@ -33,6 +34,13 @@ function varargout = computeFTC(spikeTimes, freqs, analysisWindow, varargin)
 % 		curveStruct.mean_ci		cell array of 95% conf. intervals for mean
 % 		curveStruct.median		median spike counts at each freq
 % 		curveStruct.median_ci	cell array of 95% conf interval for median
+%  spikeCount     {nfreqs, 1} cell array of spike counts per trial
+%                 each element is [ntrials, 1] vector
+%  bootData       struct with bootstat data returned by bootci for the
+%                 bootstrap estimates for mean and median confidence
+%                 intervals
+%      bootData.mean     mean statistic from bootstrap 
+%      bootData.median   median statistic from bootstrap
 %------------------------------------------------------------------------
 % See Also: plotCurveAndCI, optoproc, opto
 %------------------------------------------------------------------------
@@ -48,6 +56,7 @@ function varargout = computeFTC(spikeTimes, freqs, analysisWindow, varargin)
 %	28 Mar 2019 (SJS): updated to output curveStruct for use with
 %								plotCurveAndCI()
 %	3 Oct 2020 (SJS): added optional nbootstrap input
+%  4 Jan 2022 (SJS): returning bootData from bootci, also updated comments
 %------------------------------------------------------------------------
 
 if isempty(varargin)
@@ -58,6 +67,10 @@ end
 
 nFreqs = length(freqs);
 spikeCount = cell(nFreqs, 1);
+bootData = struct('mean', [], 'median', []);
+bootData.mean = cell(nFreqs, 1);
+bootData.median = cell(nFreqs, 1);
+
 % convert frequency from Hz to kHz
 curveStruct.xdata = 0.001*freqs;
 curveStruct.xlabel = 'Frequency (kHz)';
@@ -72,19 +85,20 @@ for n = 1:nFreqs
 	nReps = length(spikeTimes{n});
 	spikeCount{n} = zeros(nReps, 1);
 	for r = 1:nReps
-		spikeCount{n}(r) = sum(between(spikeTimes{n}{r}, analysisWindow(1), ...
+		spikeCount{n}(r) = ...
+         sum(between(spikeTimes{n}{r}, analysisWindow(1), ...
 																analysisWindow(2) ) );
 	end
 	curveStruct.mean(n) = mean(spikeCount{n}); 
 	curveStruct.std(n) = std(spikeCount{n});
-	curveStruct.mean_ci{n} = bootci(nbootstrap, @mean, spikeCount{n});
+	[curveStruct.mean_ci{n}, bootData.mean{n}] = ...
+                                 bootci(nbootstrap, @mean, spikeCount{n});
 	curveStruct.median(n) = median(spikeCount{n});
-	curveStruct.median_ci{n} = bootci(nbootstrap, @median, spikeCount{n});
+	[curveStruct.median_ci{n}, bootData.median{n}] = ...
+                           bootci(nbootstrap, @median, spikeCount{n});
 end
 curveStruct.spikeCount = spikeCount;
 
 varargout{1} = curveStruct;
-
-if nargout > 1
-	varargout{2} = spikeCount;
-end
+varargout{2} = spikeCount;
+varargout{3} = bootData;
