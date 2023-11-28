032208f62124c754cad414a2c69cb9df7b4f51af
Sharad Shanbhag <sshanbhag@neomed.edu>
2023-11-28 12:57:41 -0500

    added documentation to draw_stim_onoffset

--- draw_stim_onoffset.m
+++ draw_stim_onoffset.m
@@ -1,12 +1,30 @@
 function lH = draw_stim_onoffset(stimulus_times)
+%------------------------------------------------------------------------
+% lH = draw_stim_onoffset(stimulus_times)
+%------------------------------------------------------------------------
+% OptoAnalysis
+%------------------------------------------------------------------------
+% given stimulus_times input struct, adds lines to plot that
+% indicate stimulus onset and offset
+%------------------------------------------------------------------------
 
+%------------------------------------------------------------------------
+% Created: ??? (SJS)
+% 
+% Revisions:
+%  28 Nov 2023 (SJS): added some documentation
+%------------------------------------------------------------------------
+
+% default line color
 default_color = 'b';
 
+% check input data
 if ~isstruct(stimulus_times)
 	error('%s: stimulus_times must be a struct!', mfilename);
 elseif ~all(isfield(stimulus_times, {'onset', 'offset'}))
 	error('%s: stimulus_times must have onset and offset fields', mfilename);
 else
+   % get some information about inputs
 	nstim = length(stimulus_times);
 	lH = cell(nstim, 1);
 	% set color if needed
